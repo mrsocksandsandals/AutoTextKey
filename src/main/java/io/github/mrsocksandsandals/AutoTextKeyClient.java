7493c1433a488d605bf7815ac3aa0426a45223df
Brett I <mcdonalds.burger.dev@gmail.com>
2022-04-13 16:55:09 -0400

    add keybinding to send message to chat

--- src/main/java/io/github/mrsocksandsandals/AutoTextKeyClient.java
+++ src/main/java/io/github/mrsocksandsandals/AutoTextKeyClient.java
@@ -2,6 +2,10 @@ package io.github.mrsocksandsandals;
 
 // Imports:
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,9 +13,26 @@ public class AutoTextKeyClient implements ClientModInitializer {
 	// Define our logger.
 	public static final Logger LOGGER = LoggerFactory.getLogger("autotxt");
 
+	// Get the message we're sending to chat.
+	private static final String AUTO_TEXT_MESSAGE = "gg";
+
 	@Override
 	public void onInitializeClient() {
 		// Print an init message.
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Initializing AutoTextKey...");
+
+		// Register our keybinding.
+		LOGGER.info("Registering keybinding...");
+		ClientTickEvents.END_CLIENT_TICK.register(c -> {
+			while (autoTextKey.wasPressed()) {
+				MinecraftClient.getInstance().player.sendChatMessage(AUTO_TEXT_MESSAGE);
+			}
+		});
 	}
+
+	private KeyBinding autoTextKey = new KeyBinding(
+		"key.autotxt.autotextkey",
+		GLFW.GLFW_KEY_DELETE,
+		"category.autotxt"
+	);
 }
