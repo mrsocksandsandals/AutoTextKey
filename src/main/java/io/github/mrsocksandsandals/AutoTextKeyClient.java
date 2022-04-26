32b4423c84c0c98930e841d1aef9f0a98544490c
Brett I <mcdonalds.burger.dev@gmail.com>
2022-04-26 11:21:54 -0400

    add proper error handling, multikey support, ready for v1.0

--- src/main/java/io/github/mrsocksandsandals/AutoTextKeyClient.java
+++ src/main/java/io/github/mrsocksandsandals/AutoTextKeyClient.java
@@ -1,10 +1,14 @@
 package io.github.mrsocksandsandals;
 
 // Imports:
+import java.lang.ArrayIndexOutOfBoundsException;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.network.MessageType;
+import net.minecraft.text.LiteralText;
+
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,23 +35,58 @@ public class AutoTextKeyClient implements ClientModInitializer {
 		// Register our keybinding.
 		LOGGER.info("Registering keybinding...");
 		ClientTickEvents.END_CLIENT_TICK.register(c -> {
+			// We have to get the UUID every time, because the player isn't initialized before this point.
+			// Otherwise, I would keep it in a constant.
+
 			// Print the first message.
-			if (autoTextKey0.wasPressed()) {
-				CLIENT.player.sendChatMessage(messages[0]);
+			while (autoTextKey0.wasPressed()) {
+				try {
+					CLIENT.player.sendChatMessage(messages[0]);
+				} catch (ArrayIndexOutOfBoundsException e) {
+					LOGGER.error("messages[0] is unset!");
+					LOGGER.error("not sending...");
+					// Print an error message to the player only.
+					CLIENT.inGameHud.addChatMessage(
+						MessageType.SYSTEM,
+						new LiteralText("You haven't defined the first message!"),
+						CLIENT.player.getUuid()
+					);
+				}
 			}
 
 			// Print the second message.
-			if (autoTextKey1.wasPressed()) {
-				CLIENT.player.sendChatMessage(messages[1]);
+			while (autoTextKey1.wasPressed()) {
+				try {
+					CLIENT.player.sendChatMessage(messages[1]);
+				} catch (ArrayIndexOutOfBoundsException e) {
+					LOGGER.error("messages[1] is unset!");
+					LOGGER.error("not sending...");
+					CLIENT.inGameHud.addChatMessage(
+						MessageType.SYSTEM,
+						new LiteralText("You haven't defined the second message!"),
+						CLIENT.player.getUuid()
+					);
+				}
 			}
 
 			// Print the third message.
-			if (autoTextKey2.wasPressed()) {
-				CLIENT.player.sendChatMessage(messages[2]);
+			while (autoTextKey2.wasPressed()) {
+				try {
+					CLIENT.player.sendChatMessage(messages[2]);
+				} catch (ArrayIndexOutOfBoundsException e) {
+					LOGGER.error("messages[2] is unset!");
+					LOGGER.error("not sending...");
+					CLIENT.inGameHud.addChatMessage(
+						MessageType.SYSTEM,
+						new LiteralText("You haven't defined the third message!"),
+						CLIENT.player.getUuid()
+					);
+				}
 			}
 		});
 	}
 
+	// Create keybindings (translation key, keycode, category)
 	private KeyBinding autoTextKey0 = new KeyBinding(
 		"key.autotxt.key0",
 		GLFW.GLFW_KEY_DELETE,
